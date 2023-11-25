d243bb2e66f24e8e1c203aaf603a5c01f56d0411
Gitugayathri <142978822+Gitugayathri@users.noreply.github.com>
2023-11-25 07:46:21 +0000

    fff

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def hey():
         sepal_width = float(request.form['sepal_width'])
         petal_length = float(request.form['petal_length'])
         petal_width = float(request.form['petal_width'])
-    with open('model.pkl', 'rb') as file:
-        loaded_model = pickle.load(file)
+        model = pickle.load(open('model.pkl','rb'))
+        
         user_input = [[sepal_length, sepal_width, petal_length, petal_width]]
         prediction = model.predict([[float(user_input)]])
         predicted_species = iris.target_names[prediction][0]
